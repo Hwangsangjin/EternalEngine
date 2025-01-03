1e9a60d6046a81a710a36321330491601941d4a4
Hwangsangjin <hwangsangjin88@gmail.com>
2025-01-03 12:38:58 +0900

    오브젝트 클래스 추가

---
+++ Engine/Object/Object.cpp
@@ -0,0 +1,19 @@
+#include "Object.h"
+
+CObject::CObject()
+	: bActive{true}
+	, bExpired{false}
+{
+}
+
+CObject::~CObject()
+{
+}
+
+void CObject::Update(float DeltaTime)
+{
+}
+
+void CObject::Render()
+{
+}
---
+++ Engine/Object/Object.h
@@ -0,0 +1,21 @@
+#pragma once
+
+// 오브젝트 클래스
+class CObject
+{
+public:
+	CObject();
+	virtual ~CObject();
+
+	// 루프 처리 함수
+	virtual void Update(float DeltaTime);
+	virtual void Render();
+
+protected:
+	// 활성화 상태인지 나타내는 변수
+	bool bActive;
+
+	// 제거 요청이 됐는지 여부를 나타내는 변수
+	bool bExpired;
+};
+
