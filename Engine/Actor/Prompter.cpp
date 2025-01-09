548bde43d76508cc2f6cb5136b2f47bb0d7ba575
Hwangsangjin <hwangsangjin88@gmail.com>
2025-01-09 16:38:32 +0900

    프롬프터 클래스 추가

---
+++ Engine/Actor/Prompter.cpp
@@ -0,0 +1,33 @@
+﻿#include "Precompiled.h"
+#include "Prompter.h"
+#include "Engine/Engine.h"
+
+CPrompter::CPrompter(char Image)
+	: Image(Image)
+{
+}
+
+CPrompter::~CPrompter()
+{
+}
+
+void CPrompter::Render()
+{
+	Super::Render();
+
+	// #1 콘솔 좌표 옮기기
+	CEngine::Get().SetCursorPosition(Position);
+
+	// #2 콘솔 출력
+	Log("%c", Image);
+}
+
+void CPrompter::SetPosition(const FVector2& NewPosition)
+{
+	// 이전의 위치를 먼저 지우기
+	CEngine::Get().SetCursorPosition(Position);
+	Log(" ");
+
+	// 새로운 위치로 옮기기
+	Super::SetPosition(NewPosition);
+}
---
+++ Engine/Actor/Prompter.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+#include "Actor.h"
+
+class ENGINE_API CPrompter : public CActor
+{
+	RTTI_DECL(CPrompter, CActor)
+
+public:
+	CPrompter(char Image = ' ');
+	virtual ~CPrompter();
+
+	virtual void Render() override;
+	virtual void SetPosition(const FVector2& NewPosition) override;
+
+private:
+	// 화면에 그릴 문자 값
+	char Image;
+};
