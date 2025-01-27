c834d9080c4d69353b064892c1e00759f5e03e28
Hwangsangjin <hwangsangjin88@gmail.com>
2025-01-27 21:02:45 +0900

    타이머 클래스 추가

---
+++ Engine/Timer/Timer.cpp
@@ -0,0 +1,37 @@
+﻿#include "Precompiled.h"
+#include "Timer.h"
+#include <chrono>
+
+CTimer::CTimer()
+	: Frequency()
+	, CurrentTime()
+	, PreviousTime()
+	, TargetDeltaTime(0.0f)
+	, DeltaTime(0.0f)
+	, FPS(0.0f)
+{
+	QueryPerformanceFrequency(&Frequency);
+	QueryPerformanceCounter(&PreviousTime);
+}
+
+float CTimer::CalculateDeltaTime()
+{
+	do
+	{
+		// 현재 시간 측정
+		QueryPerformanceCounter(&CurrentTime);
+
+		// 델타 타임 계산 (현재 시간과 이전 시간 차이를 주파수로 나누기)
+		DeltaTime = static_cast<float>(CurrentTime.QuadPart - PreviousTime.QuadPart) / static_cast<float>(Frequency.QuadPart);
+
+	} while (DeltaTime < TargetDeltaTime); // 프레임이 너무 빠르게 진행되지 않도록 제한
+
+	// 이전 시간을 현재 시간으로 업데이트
+	PreviousTime = CurrentTime;
+
+	// 초당 프레임 계산
+	FPS = 1.0f / DeltaTime;
+	 
+	// 델타 타임 반환
+	return DeltaTime;
+}
---
+++ Engine/Timer/Timer.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+class ENGINE_API CTimer
+{
+public:
+	CTimer();
+
+	float CalculateDeltaTime();
+
+	FORCEINLINE float GetDeltaTime() const { return DeltaTime; }
+	FORCEINLINE float GetFPS() const { return FPS; }
+	FORCEINLINE void SetTargetFPS(float InFPS) { TargetDeltaTime = 1.0f / InFPS; }
+
+private:
+	LARGE_INTEGER Frequency;
+	LARGE_INTEGER CurrentTime;
+	LARGE_INTEGER PreviousTime;
+	float TargetDeltaTime;
+	float DeltaTime;
+	float FPS;
+};
