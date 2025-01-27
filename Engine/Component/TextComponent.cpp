3ccc5383b3129d2cf0818a86aa60e377afa39a34
Hwangsangjin <hwangsangjin88@gmail.com>
2025-01-27 21:24:19 +0900

    텍스트 컴포넌트 클래스 추가

---
+++ Engine/Component/TextComponent.cpp
@@ -0,0 +1,19 @@
+﻿#include "Precompiled.h"
+#include "TextComponent.h"
+#include "Actor/Actor.h"
+#include "Core/Core.h"
+#include "Renderer/Renderer.h"
+
+CTextComponent::CTextComponent(CActor* InActor, const FString& InName)
+	: Super(InActor, InName)
+	, String(TEXT(""))
+	, Color(EColor::White)
+{
+}
+
+void CTextComponent::Render()
+{
+	Super::Render();
+
+	CCore::Get().GetRenderer()->PrintText(Owner->GetPosition(), String, Color);
+}
---
+++ Engine/Component/TextComponent.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include "ActorComponent.h"
+
+class ENGINE_API CTextComponent : public CActorComponent
+{
+	RTTI_DECL(CTextComponent, CActorComponent)
+
+public:
+	CTextComponent(CActor* InActor, const FString& InName);
+	virtual ~CTextComponent() = default;
+
+	virtual void Render() override;
+
+	FORCEINLINE const FString& GetString() const { return String; }
+	FORCEINLINE void SetString(const FString& InString) { String = InString; }
+
+	FORCEINLINE const EColor& GetColor() const { return Color; }
+	FORCEINLINE void SetColor(const EColor& InColor) { Color = InColor; }
+
+private:
+	// 문자열
+	FString String;
+
+	// 색상
+	EColor Color;
+};
