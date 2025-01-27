30bfbd5625e4b10f870852d51b672e6db31b518e
Hwangsangjin <hwangsangjin88@gmail.com>
2025-01-27 21:23:29 +0900

    액터 컴포넌트 클래스 추가

---
+++ Engine/Component/ActorComponent.cpp
@@ -0,0 +1,9 @@
+﻿#include "Precompiled.h"
+#include "ActorComponent.h"
+#include "Actor/Actor.h"
+
+CActorComponent::CActorComponent(CActor* InActor, const FString& InName)
+    : Owner(InActor)
+    , Name(InName)
+{
+}
---
+++ Engine/Component/ActorComponent.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+
+#include "Object/Object.h"
+
+class CActor;
+
+class ENGINE_API CActorComponent : public CObject
+{
+    RTTI_DECL(CActorComponent, CObject)
+
+public:
+    // 생성자에서 소유자 액터와 컴포넌트 이름 초기화
+    CActorComponent(CActor* InActor, const FString& InName);
+    virtual ~CActorComponent() = default;
+
+    // 가상 함수로, 실제 동작은 컴포넌트 별로 구현
+    virtual void Update(float DeltaTime) {}
+    virtual void Render() {}
+
+    // 소유자 액터 관련 함수
+    FORCEINLINE CActor* GetOwner() const { return Owner; }
+    FORCEINLINE void SetOwner(CActor* InActor) { Owner = InActor; }
+
+    // 컴포넌트 이름 관련 함수
+    FORCEINLINE const FString& GetName() const { return Name; }
+    FORCEINLINE void SetName(const FString& InName) { Name = InName; }
+
+protected:
+    // 소유자 액터
+    CActor* Owner;
+
+    // 컴포넌트 이름
+    FString Name;
+};
