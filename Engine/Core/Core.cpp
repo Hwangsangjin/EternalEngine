ba11a5d40322b4bf4ef6eab8a95c8a07cb17a178
Hwangsangjin <hwangsangjin88@gmail.com>
2025-01-27 21:34:27 +0900

    코어 클래스 추가

---
+++ Engine/Core/Core.cpp
@@ -0,0 +1,92 @@
+﻿#include "Precompiled.h"
+#include "Core.h"
+#include "Level/Level.h"
+#include "Actor/Actor.h"
+#include "Timer/Timer.h"
+#include "Input/Input.h"
+#include "World/World.h"
+#include "Renderer/Renderer.h"
+
+// 스태틱 변수 초기화
+CCore* CCore::Instance = nullptr;
+
+CCore::CCore()
+	: bRunning(true)
+	, Timer(new CTimer)
+	, Input(new CInput)
+	, World(new CWorld)
+	, Renderer(new CRenderer)
+{
+	// 싱글톤 객체 설정
+	Instance = this;
+
+	// 랜덤 시드 설정
+	srand((unsigned int)time(nullptr));
+}
+
+CCore::~CCore()
+{
+	delete Timer;
+	Timer = nullptr;
+
+	delete Input;
+	Input = nullptr;
+
+	delete World;
+	World = nullptr;
+
+	delete Renderer;
+	Renderer = nullptr;
+}
+
+CCore& CCore::Get()
+{
+	return *Instance;
+}
+
+void CCore::Run()
+{
+	// 퍼시스턴트 레벨이 없는 경우 종료
+	if (!World->GetPersistentLevel())
+		return;
+
+	// 목표 프레임 설정
+	Timer->SetTargetFPS(60.0f);
+
+	// 게임 루프
+	while (bRunning)
+	{
+		// 델타 타임 계산
+		const float DeltaTime = Timer->CalculateDeltaTime();
+		
+		// 입력 처리
+		Input->ProcessInput();
+
+		// 업데이트
+		World->Update(DeltaTime);
+
+		// 렌더
+		Renderer->Render();
+	}
+}
+
+void CCore::QuitGame()
+{
+	// 실행 플래그 설정
+	bRunning = false;
+}
+
+void CCore::LoadLevel(CLevel* InLevel)
+{
+	World->LoadLevel(InLevel);
+}
+
+void CCore::AddActor(CActor* InActor)
+{
+	World->GetPersistentLevel()->AddActor(InActor);
+}
+
+void CCore::RemoveActor(CActor* InActor)
+{
+	World->GetPersistentLevel()->RemoveActor(InActor);
+}
---
+++ Engine/Core/Core.h
@@ -0,0 +1,55 @@
+﻿#pragma once
+
+class CLevel;
+class CActor;
+class CTimer;
+class CInput;
+class CWorld;
+class CRenderer;
+
+class ENGINE_API CCore
+{
+public:
+	CCore();
+	virtual ~CCore();
+
+	// 싱글톤 객체 접근 함수
+	static CCore& Get();
+
+	// 게임 실행 함수
+	void Run();
+
+	// 게임 종료 함수
+	void QuitGame();
+
+	// 레벨 추가 함수
+	void LoadLevel(CLevel* InLevel);
+
+	// 액터 추가, 삭제 함수
+	void AddActor(CActor* InActor);
+	void RemoveActor(CActor* InActor);
+
+	FORCEINLINE CTimer* GetTimer() const { return Timer; }
+	FORCEINLINE CInput* GetInput() const { return Input; }
+	FORCEINLINE CWorld* GetWorld() const { return World; }
+	FORCEINLINE CRenderer* GetRenderer() const { return Renderer; }
+
+protected:
+	// 싱글톤 구현을 위한 정적 인스턴스
+	static CCore* Instance;
+
+	// 게임 실행 플래그
+	bool bRunning;
+
+	// 타이머
+	CTimer* Timer;
+
+	// 입력
+	CInput* Input;
+
+	// 월드
+	CWorld* World;
+
+	// 렌더러
+	CRenderer* Renderer;
+};
