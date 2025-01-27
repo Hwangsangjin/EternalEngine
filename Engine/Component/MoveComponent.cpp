91fea6226dcae64ecd71362dc9bc08d1672a176f
Hwangsangjin <hwangsangjin88@gmail.com>
2025-01-27 21:27:32 +0900

    이동 컴포넌트 클래스 추가

---
+++ Engine/Component/MoveComponent.cpp
@@ -0,0 +1,44 @@
+﻿#include "Precompiled.h"
+#include "MoveComponent.h"
+#include "Actor/Actor.h"
+
+CMoveComponent::CMoveComponent(CActor* InActor, const FString& InName)
+	: Super(InActor, InName)
+	, AngularSpeed(0.0f)
+	, ForwardSpeed(0.0f)
+	, RightSpeed(0.0f)
+{
+}
+
+void CMoveComponent::Update(float DeltaTime)
+{
+	Super::Update(DeltaTime);
+
+	// 예외 처리
+	if (Owner->GetState() == CActor::EState::Paused)
+		return;
+
+	// 회전 처리
+	if (!FMath::IsNearlyZero(AngularSpeed))
+	{
+		float Rotation = Owner->GetRotation();
+		Rotation += AngularSpeed * DeltaTime;
+		Owner->SetRotation(Rotation);
+	}
+
+	// 상하 이동 처리
+	if (!FMath::IsNearlyZero(ForwardSpeed))
+	{
+		FVector2 Position = Owner->GetPosition();
+		Position += Owner->GetForwardVector() * ForwardSpeed * DeltaTime;
+		Owner->SetPosition(Position);
+	}
+
+	// 좌우 이동 처리
+	if (!FMath::IsNearlyZero(RightSpeed))
+	{
+		FVector2 Position = Owner->GetPosition();
+		Position += Owner->GetRightVector() * RightSpeed * DeltaTime;
+		Owner->SetPosition(Position);
+	}
+}
---
+++ Engine/Component/MoveComponent.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+
+#include "ActorComponent.h"
+
+class ENGINE_API CMoveComponent : public CActorComponent
+{
+	RTTI_DECL(CMoveComponent, CActorComponent)
+
+public:
+	CMoveComponent(CActor* InActor, const FString& InName);
+	virtual ~CMoveComponent() = default;
+
+	virtual void Update(float DeltaTime) override;
+
+	FORCEINLINE float GetAngularSpeed() const { return AngularSpeed; }
+	FORCEINLINE void SetAngularSpeed(float InAngularSpeed) { AngularSpeed = InAngularSpeed; }
+
+	FORCEINLINE float GetForwardSpeed() const { return ForwardSpeed; }
+	FORCEINLINE void SetForwardSpeed(float InForwardSpeed) { ForwardSpeed = InForwardSpeed; }
+
+	FORCEINLINE float GetRightSpeed() const { return RightSpeed; }
+	FORCEINLINE void SetRightSpeed(float InRightSpeed) { RightSpeed = InRightSpeed; }
+
+private:
+	// 회전 속도
+	float AngularSpeed;
+
+	// 전방 속도
+	float ForwardSpeed;
+
+	// 우측 속도
+	float RightSpeed;
+};
