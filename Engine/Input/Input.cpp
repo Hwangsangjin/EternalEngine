7573bf274145a329b0633eb452910653f6082b2a
Hwangsangjin <hwangsangjin88@gmail.com>
2025-01-27 21:16:39 +0900

    입력 클래스 추가

---
+++ Engine/Input/Input.cpp
@@ -0,0 +1,43 @@
+﻿#include "Precompiled.h"
+#include "Input.h"
+
+CInput::CInput()
+{
+	KeyStates.fill(EKeyState::None);
+}
+
+void CInput::ProcessInput()
+{
+	for (SHORT i = 0; i < KeyStates.size(); ++i)
+	{
+		if (GetAsyncKeyState(i) & 0x8000)
+		{
+			if (KeyStates[i] == EKeyState::None || KeyStates[i] == EKeyState::Up)
+				KeyStates[i] = EKeyState::Down;
+			else
+				KeyStates[i] = EKeyState::Hold;
+		}
+		else
+		{
+			if (KeyStates[i] == EKeyState::Down || KeyStates[i] == EKeyState::Hold)
+				KeyStates[i] = EKeyState::Up;
+			else
+				KeyStates[i] = EKeyState::None;
+		}
+	}
+}
+
+bool CInput::GetKey(size_t InIndex) const
+{
+	return KeyStates[InIndex] == EKeyState::Down || KeyStates[InIndex] == EKeyState::Hold;
+}
+
+bool CInput::GetKeyDown(size_t InIndex) const
+{
+	return KeyStates[InIndex] == EKeyState::Down;
+}
+
+bool CInput::GetKeyUp(size_t InIndex) const
+{
+	return KeyStates[InIndex] == EKeyState::Up;
+}
---
+++ Engine/Input/Input.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+class ENGINE_API CInput
+{
+public:
+	CInput();
+
+	// 키 상태 갱신
+	void ProcessInput();
+
+	// 키가 눌린 상태인지 확인
+	bool GetKey(size_t InIndex) const;
+
+	// 키가 방금 눌린 상태인지 확인
+	bool GetKeyDown(size_t InIndex) const;
+
+	// 키가 방금 떼어진 상태인지 확인
+	bool GetKeyUp(size_t InIndex) const;
+
+private:
+	// 키 상태 배열
+	std::array<EKeyState, 256> KeyStates;
+};
