903bd4b5f14cde0fee1b8904e5ad776fee3a4913
Hwangsangjin <hwangsangjin88@gmail.com>
2025-01-27 21:28:36 +0900

    입력 컴포넌트 클래스 추가

---
+++ Engine/Component/InputComponent.cpp
@@ -0,0 +1,56 @@
+﻿#include "Precompiled.h"
+#include "InputComponent.h"
+#include "Core/Core.h"
+#include "Input/Input.h"
+
+CInputComponent::CInputComponent(CActor* InActor, const FString& InName)
+	: Super(InActor, InName)
+	, ForwardAcceleration(0.0f)
+	, RightAcceleration(0.0f)
+	, AngularAcceleration(0.0f)
+	, ForwardKey(0)
+	, BackwardKey(0)
+	, LeftKey(0)
+	, RightKey(0)
+	, ClockwiseKey(0)
+	, CounterClockwiseKey(0)
+{
+}
+
+void CInputComponent::Update(float DeltaTime)
+{
+    Super::Update(DeltaTime);
+
+	// 각 속도 계산
+	float AngularSpeed = 0.0f;
+
+	if (CCore::Get().GetInput()->GetKey(ClockwiseKey))
+		AngularSpeed += AngularAcceleration * DeltaTime;
+
+	if (CCore::Get().GetInput()->GetKey(CounterClockwiseKey))
+		AngularSpeed -= AngularAcceleration * DeltaTime;
+
+	SetAngularSpeed(AngularSpeed);
+
+	// 전방 속도 계산
+	float ForwardSpeed = 0.0f;
+
+	if (CCore::Get().GetInput()->GetKey(ForwardKey))
+		ForwardSpeed += ForwardAcceleration * DeltaTime;
+
+	if (CCore::Get().GetInput()->GetKey(BackwardKey))
+		ForwardSpeed -= ForwardAcceleration * DeltaTime;
+
+	SetForwardSpeed(ForwardSpeed);
+
+	// 우측 속도 계산
+	float RightSpeed = 0.0f;
+
+	if (CCore::Get().GetInput()->GetKey(LeftKey))
+		RightSpeed -= RightAcceleration * DeltaTime;
+
+	if (CCore::Get().GetInput()->GetKey(RightKey))
+		RightSpeed += RightAcceleration * DeltaTime;
+
+	SetRightSpeed(RightSpeed);
+}
---
+++ Engine/Component/InputComponent.h
@@ -0,0 +1,61 @@
+﻿#pragma once
+
+#include "MoveComponent.h"
+
+class ENGINE_API CInputComponent : public CMoveComponent
+{
+	RTTI_DECL(CInputComponent, CMoveComponent)
+
+public:
+	CInputComponent(CActor* InActor, const FString& InName);
+	virtual ~CInputComponent() = default;
+
+	virtual void Update(float DeltaTime) override;
+
+	FORCEINLINE float GetAngularAcceleration() const { return AngularAcceleration; }
+	FORCEINLINE void SetAngularAcceleration(float InAngularAcceleration) { AngularAcceleration = InAngularAcceleration; }
+
+	FORCEINLINE int GetClockwiseKey() const { return ClockwiseKey; }
+	FORCEINLINE void SetClockwiseKey(int InClockwiseKey) { ClockwiseKey = InClockwiseKey; }
+
+	FORCEINLINE int GetCounterClockwiseKey() const { return CounterClockwiseKey; }
+	FORCEINLINE void SetCounterClockwiseKey(int InCounterClockwiseKey) { CounterClockwiseKey = InCounterClockwiseKey; }
+
+	FORCEINLINE float GetForwardAcceleration() const { return ForwardAcceleration; }
+	FORCEINLINE void SetForwardAcceleration(float InForwardAcceleration) { ForwardAcceleration = InForwardAcceleration; }
+
+	FORCEINLINE float GetRightAcceleration() const { return RightAcceleration; }
+	FORCEINLINE void SetRightAcceleration(float InRightAcceleration) { RightAcceleration = InRightAcceleration; }
+
+	FORCEINLINE int GetForwardKey() const { return ForwardKey; }
+	FORCEINLINE void SetForwardKey(int InForwardKey) { ForwardKey = InForwardKey; }
+
+	FORCEINLINE int GetBackwardKey() const { return BackwardKey; }
+	FORCEINLINE void SetBackwardKey(int InBackwardKey) { BackwardKey = InBackwardKey; }
+
+	FORCEINLINE int GetLeftKey() const { return LeftKey; }
+	FORCEINLINE void SetLeftKey(int InLeftKey) { LeftKey = InLeftKey; }
+
+	FORCEINLINE int GetRightKey() const { return RightKey; }
+	FORCEINLINE void SetRightKey(int InRightKey) { RightKey = InRightKey; }
+
+private:
+	// 회전 가속도
+	float AngularAcceleration;
+
+	// 회전을 위한 키
+	int ClockwiseKey;
+	int CounterClockwiseKey;
+
+	// 전방 가속도
+	float ForwardAcceleration;
+
+	// 우측 가속도
+	float RightAcceleration;
+
+	// 이동을 위한 키
+	int ForwardKey;
+	int BackwardKey;
+	int LeftKey;
+	int RightKey;
+};
