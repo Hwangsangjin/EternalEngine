7b84a1f13753bf84cc46d30c12225654c9fde660
Hwangsangjin <hwangsangjin88@gmail.com>
2025-02-15 22:29:17 +0900

    정적 메시 클래스 추가

---
+++ Engine/Mesh/StaticMesh.cpp
@@ -0,0 +1,48 @@
+﻿#include "Precompiled.h"
+#include "StaticMesh.h"
+#include "Vertex/Vertex.h"
+#include "Material/Material.h"
+#include "Material/DirectX11Material.h"
+#include "Buffer/DirectX11VertexBuffer.h"
+#include "Buffer/DirectX11IndexBuffer.h"
+#include "Buffer/VertexBuffer.h"
+#include "Buffer/IndexBuffer.h"
+#include "Core/Core.h"
+#include "System/RenderSystem.h"
+
+FStaticMesh::FStaticMesh(const std::vector<FVertex>& InVertices, const std::vector<UINT>& InIndices)
+	: Vertices(InVertices)
+	, Indices(InIndices)
+	, VertexBuffer(nullptr)
+	, IndexBuffer(nullptr)
+{
+	Vertices.assign(InVertices.begin(), InVertices.end());
+	Indices.assign(InIndices.begin(), InIndices.end());
+
+	VertexBuffer = CCore::Get().GetRenderSystem()->CreateVertexBuffer(sizeof(FVertex) * static_cast<UINT>(Vertices.size()), Vertices.data());
+	IndexBuffer = CCore::Get().GetRenderSystem()->CreateIndexBuffer(sizeof(UINT) * static_cast<UINT>(Indices.size()), Indices.data());
+}
+
+FStaticMesh::~FStaticMesh()
+{
+	if (VertexBuffer)
+	{
+		delete VertexBuffer;
+		VertexBuffer = nullptr;
+	}
+
+	if (IndexBuffer)
+	{
+		delete IndexBuffer;
+		IndexBuffer = nullptr;
+	}
+}
+
+void FStaticMesh::Bind()
+{
+	UINT Stride = sizeof(FVertex);
+	UINT Offset = 0;
+
+	CCore::Get().GetRenderSystem()->SetVertexBuffer(VertexBuffer, Stride, Offset);
+	CCore::Get().GetRenderSystem()->SetIndexBuffer(IndexBuffer);
+}
---
+++ Engine/Mesh/StaticMesh.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+
+#include "Vertex/Vertex.h"
+
+class FVertexBuffer;
+class FIndexBuffer;
+
+class ENGINE_API FStaticMesh
+{
+public:
+	FStaticMesh(const std::vector<FVertex>& InVertices, const std::vector<UINT>& InIndices);
+	~FStaticMesh();
+
+	void Bind();
+
+	FORCEINLINE UINT GetIndexCount() const { return static_cast<UINT>(Indices.size()); }
+
+private:
+	// 정점 데이터
+	std::vector<FVertex> Vertices;
+
+	// 정점 버퍼
+	FVertexBuffer* VertexBuffer;
+
+	// 인덱스 데이터
+	std::vector<UINT> Indices;
+
+	// 인덱스 버퍼
+	FIndexBuffer* IndexBuffer;
+};
