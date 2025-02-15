aef7aafd6c1d9a2c163c7a1b2cee71662a33066c
Hwangsangjin <hwangsangjin88@gmail.com>
2025-02-15 22:28:54 +0900

    정점 클래스 추가

---
+++ Engine/Vertex/Vertex.cpp
@@ -0,0 +1,8 @@
+﻿#include "Precompiled.h"
+#include "Vertex.h"
+
+FVertex::FVertex(const FVector3& InPosition, const FVector3& InColor)
+	: Position(InPosition)
+	, Color(InColor)
+{
+}
---
+++ Engine/Vertex/Vertex.h
@@ -0,0 +1,9 @@
+﻿#pragma once
+
+struct ENGINE_API FVertex
+{
+	FVertex(const FVector3& InPosition, const FVector3& InColor);
+
+	FVector3 Position;
+	FVector3 Color;
+};
