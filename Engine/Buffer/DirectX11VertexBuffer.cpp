9ec2e9ba689070df55f004b0a244da8c31ddb4d7
Hwangsangjin <hwangsangjin88@gmail.com>
2025-02-15 22:32:25 +0900

    DX11 정점 버퍼 추가

---
+++ Engine/Buffer/DirectX11VertexBuffer.cpp
@@ -0,0 +1,16 @@
+﻿#include "Precompiled.h"
+#include "DirectX11VertexBuffer.h"
+
+FDirectX11VertexBuffer::FDirectX11VertexBuffer(ID3D11Buffer* InVertexBuffer)
+	: VertexBuffer(InVertexBuffer)
+{
+}
+
+FDirectX11VertexBuffer::~FDirectX11VertexBuffer()
+{
+    if (VertexBuffer)
+    {
+        VertexBuffer->Release();
+        VertexBuffer = nullptr;
+    }
+}
---
+++ Engine/Buffer/DirectX11VertexBuffer.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+#include "VertexBuffer.h"
+
+class ENGINE_API FDirectX11VertexBuffer : public FVertexBuffer
+{
+public:
+    FDirectX11VertexBuffer(ID3D11Buffer* InVertexBuffer);
+    virtual ~FDirectX11VertexBuffer();
+
+    virtual void* GetBuffer() const override { return static_cast<void*>(VertexBuffer); }
+
+private:
+    ID3D11Buffer* VertexBuffer;
+};
