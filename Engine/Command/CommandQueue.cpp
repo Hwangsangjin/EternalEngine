abaea6c11b31404401432d157b26b9b947dd2a71
Hwangsangjin <hwangsangjin88@gmail.com>
2025-02-15 22:34:10 +0900

    커맨드 큐 클래스 추가

---
+++ Engine/Command/CommandQueue.cpp
@@ -0,0 +1,24 @@
+﻿#include "Precompiled.h"
+#include "CommandQueue.h"
+#include "DrawCommand.h"
+
+void FCommandQueue::Enqueue(FDrawCommand* InDrawCommand)
+{
+    std::lock_guard<std::mutex> Lock(QueueMutex);
+    CommandQueue.push(InDrawCommand);
+}
+
+void FCommandQueue::ExecuteAll()
+{
+    std::lock_guard<std::mutex> Lock(QueueMutex);
+
+    while (!CommandQueue.empty())
+    {
+        // 커맨드 실행
+        CommandQueue.front()->Execute();
+
+        // 메모리 해제
+        delete CommandQueue.front();
+        CommandQueue.pop();
+    }
+}
---
+++ Engine/Command/CommandQueue.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+
+class FDrawCommand;
+
+class ENGINE_API FCommandQueue
+{
+public:
+    void Enqueue(FDrawCommand* InDrawCommand);
+    void ExecuteAll();
+
+private:
+    std::queue<FDrawCommand*> CommandQueue;
+    std::mutex QueueMutex;
+};
