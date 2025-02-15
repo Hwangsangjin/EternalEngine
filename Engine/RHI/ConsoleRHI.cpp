fd84a39fe00ba33b964657148954ceb00872c9c3
Hwangsangjin <hwangsangjin88@gmail.com>
2025-02-15 22:20:11 +0900

    콘솔 RHI 추가

---
+++ Engine/RHI/ConsoleRHI.cpp
@@ -0,0 +1,118 @@
+﻿#include "Precompiled.h"
+#include "ConsoleRHI.h"
+
+FConsoleRHI::FConsoleRHI()
+	: StdHandle(GetStdHandle(STD_OUTPUT_HANDLE))
+	, FrontBuffer(INVALID_HANDLE_VALUE)
+	, BackBuffer(INVALID_HANDLE_VALUE)
+	, BufferSize(COORD(50, 20))
+	, BufferInfo()
+{
+	// 스크린 버퍼 생성
+	FrontBuffer = CreateConsoleScreenBuffer(GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+	BackBuffer = CreateConsoleScreenBuffer(GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+
+	// 스크린 버퍼 크기 설정
+	SetConsoleScreenBufferSize(FrontBuffer, BufferSize);
+	SetConsoleScreenBufferSize(BackBuffer, BufferSize);
+
+	// 창 크기 설정
+	SMALL_RECT Rect = { 0, 0, BufferSize.X - 1, BufferSize.Y - 1 };
+	SetConsoleWindowInfo(FrontBuffer, true, &Rect);
+	SetConsoleWindowInfo(BackBuffer, true, &Rect);
+
+	// 활성 버퍼 설정
+	SetConsoleActiveScreenBuffer(FrontBuffer);
+
+	// 커서 타입 설정
+	SetCursorType(ECursorType::Hidden);
+}
+
+FConsoleRHI::~FConsoleRHI()
+{
+	// 스크린 버퍼 메모리 해제
+	if (FrontBuffer != INVALID_HANDLE_VALUE)
+	{
+		CloseHandle(FrontBuffer);
+		FrontBuffer = INVALID_HANDLE_VALUE;
+	}
+
+	if (BackBuffer != INVALID_HANDLE_VALUE)
+	{
+		CloseHandle(BackBuffer);
+		BackBuffer = INVALID_HANDLE_VALUE;
+	}
+}
+
+void FConsoleRHI::SetCursorType(const ECursorType& InCursorType)
+{
+	CONSOLE_CURSOR_INFO CursorInfo = {};
+
+	// 타입 별로 구조체 값 설정
+	switch (InCursorType)
+	{
+	case ECursorType::Hidden:
+		CursorInfo.dwSize = 1;
+		CursorInfo.bVisible = FALSE;
+		break;
+	case ECursorType::Solid:
+		CursorInfo.dwSize = 100;
+		CursorInfo.bVisible = TRUE;
+		break;
+	case ECursorType::Normal:
+		CursorInfo.dwSize = 20;
+		CursorInfo.bVisible = TRUE;
+		break;
+	}
+
+	SetConsoleCursorInfo(FrontBuffer, &CursorInfo);
+	SetConsoleCursorInfo(BackBuffer, &CursorInfo);
+}
+
+void FConsoleRHI::DrawText(const FVector2& InPosition, const FString& InText, const FLinearColor& InColor)
+{
+	// 텍스트 길이
+	const DWORD Length = static_cast<DWORD>(InText.GetWideString().length());
+
+	// 출력 위치
+	const COORD Coord = { static_cast<SHORT>(InPosition.X), static_cast<SHORT>(InPosition.Y) };
+
+	// 출력된 문자 또는 속성의 수
+	DWORD Written = 0;
+
+	// FLinearColor를 FColor32로 변환
+	FColor32 ConsoleColor = InColor.ToColor32();
+
+	// 콘솔 색상 설정
+	FillConsoleOutputAttribute(BackBuffer, ConsoleColor.ToConsoleColorAttribute(), Length, Coord, &Written);
+
+	// 문자열 전체를 한 번에 출력
+	WriteConsoleOutputCharacterW(BackBuffer, InText.GetWideString().c_str(), Length, Coord, &Written);
+}
+
+void FConsoleRHI::Clear()
+{
+	// 콘솔 버퍼 크기에 맞는 전체 문자 개수 계산
+	const DWORD Length = BufferSize.X * BufferSize.Y;
+
+	// 콘솔 좌표를 (0, 0)으로 설정 (시작 위치)
+	constexpr COORD Coord = { 0, 0 };
+
+	// 출력된 문자 또는 속성의 수
+	DWORD Written = 0;
+
+	// 백 버퍼를 공백 문자로 채우기 (콘솔에 출력된 내용을 비우기 위해)
+	FillConsoleOutputCharacter(BackBuffer, ' ', Length, Coord, &Written);
+
+	// 백 버퍼의 색상 속성 초기화 (기본 색상으로 설정)
+	FillConsoleOutputAttribute(BackBuffer, BufferInfo.wAttributes, Length, Coord, &Written);
+}
+
+void FConsoleRHI::Present()
+{
+	// 백 버퍼 활성화
+	SetConsoleActiveScreenBuffer(BackBuffer);
+
+	// 페이지 플리핑
+	std::swap(FrontBuffer, BackBuffer);
+}
---
+++ Engine/RHI/ConsoleRHI.h
@@ -0,0 +1,44 @@
+﻿#pragma once
+
+#include "RHI.h"
+
+class ENGINE_API FConsoleRHI : public FRHI
+{
+	RTTI_DECL(FConsoleRHI, FRHI)
+
+public:
+	FConsoleRHI();
+	virtual ~FConsoleRHI();
+
+	virtual void Clear() override;
+	virtual void Present() override;
+
+	// 커서 설정 함수
+	void SetCursorType(const ECursorType& InCursorType);
+
+	// 화면에 그리는 함수
+	virtual void DrawPoint(const FVector2& InPosition, const FLinearColor& InColor) override {}
+	virtual void DrawPoint(const FScreenPoint& InPosition, const FLinearColor& InColor) override {}
+	virtual void DrawLine(const FVector2& InStartPosition, const FVector2& InEndPosition, const FLinearColor& InColor) override {}
+	virtual void DrawText(const FVector2& InPosition, const FString& InText, const FLinearColor& InColor) override;
+	virtual void DrawStaticMesh(FStaticMesh* InStaticMesh, const std::vector<FMaterial*>& InMaterials) override {}
+
+protected:
+	virtual FVertexBuffer* CreateVertexBuffer(UINT Size, const void* Data) override { return nullptr; }
+	virtual FIndexBuffer* CreateIndexBuffer(UINT Size, const void* Data) override { return nullptr; }
+	virtual void SetVertexBuffer(FVertexBuffer* VertexBuffer, UINT Stride, UINT Offset) override {}
+	virtual void SetIndexBuffer(FIndexBuffer* IndexBuffer) override {}
+
+	FORCEINLINE virtual void* GetRawDevice() const override { return nullptr; }
+	FORCEINLINE virtual void* GetRawDeviceContext() const override { return nullptr; }
+
+private:
+	// 표준 디바이스 핸들
+	HANDLE StdHandle;
+
+	// 더블 버퍼링 변수
+	HANDLE FrontBuffer;
+	HANDLE BackBuffer;
+	COORD BufferSize;
+	CONSOLE_SCREEN_BUFFER_INFO BufferInfo;
+};
