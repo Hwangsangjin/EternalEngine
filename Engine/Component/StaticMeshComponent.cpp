9e850d1724f812e1e7bf9aa998b5b2d855decfb9
Hwangsangjin <hwangsangjin88@gmail.com>
2025-02-15 22:41:08 +0900

    정적 메시 컴포넌트 추가

---
+++ Engine/Component/StaticMeshComponent.cpp
@@ -0,0 +1,22 @@
+﻿#include "Precompiled.h"
+#include "StaticMeshComponent.h"
+#include "Mesh/StaticMesh.h"
+#include "Material/Material.h"
+#include "Command/DrawStaticMeshCommand.h"
+#include "Core/Core.h"
+#include "System/RenderSystem.h"
+
+CStaticMeshComponent::CStaticMeshComponent(CActor* InActor, const FString& InName)
+	: Super(InActor, InName)
+    , StaticMesh(nullptr)
+{
+}
+
+void CStaticMeshComponent::Render()
+{
+    if (!StaticMesh || Materials.empty())
+        return;
+
+    FDrawStaticMeshCommand* DrawStaticMeshCommand = new FDrawStaticMeshCommand(StaticMesh, Materials);
+    CCore::Get().GetRenderSystem()->EnqueueCommand(DrawStaticMeshCommand);
+}
---
+++ Engine/Component/StaticMeshComponent.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+
+#include "ActorComponent.h"
+
+class FStaticMesh;
+class FMaterial;
+
+class ENGINE_API CStaticMeshComponent : public CActorComponent
+{
+	RTTI_DECL(CStaticMeshComponent, CActorComponent)
+
+public:
+	CStaticMeshComponent(CActor* InActor, const FString& InName);
+	virtual ~CStaticMeshComponent() = default;
+
+	virtual void Render() override;
+
+	FORCEINLINE FStaticMesh* GetStaticMesh() const { return StaticMesh; }
+	FORCEINLINE void SetStaticMesh(FStaticMesh* InStaticMesh) { StaticMesh = InStaticMesh; }
+
+	FORCEINLINE FMaterial* GetMaterial(size_t Index) const { return Index < Materials.size() ? Materials[Index] : nullptr; }
+	FORCEINLINE const std::vector<FMaterial*>& GetMaterials() const { return Materials; }
+	FORCEINLINE void SetMaterial(size_t Index, FMaterial* InMaterial)
+	{
+		if (Index >= Materials.size())
+			Materials.resize(Index + 1, nullptr);
+
+		Materials[Index] = InMaterial;
+	}
+
+private:
+	FStaticMesh* StaticMesh;
+	std::vector<FMaterial*> Materials;
+};
