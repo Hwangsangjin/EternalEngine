b03f8c38b0a4ac34d8f2bafb52d241415fe8255b
Hwangsangjin <hwangsangjin88@gmail.com>
2025-02-15 22:38:56 +0900

    점 찍기 커맨드 추가

---
+++ Engine/Command/DrawPointCommand.cpp
@@ -0,0 +1,15 @@
+﻿#include "Precompiled.h"
+#include "DrawPointCommand.h"
+#include "Core/Core.h"
+#include "System/RenderSystem.h"
+
+FDrawPointCommand::FDrawPointCommand(const FVector2 InPosition, const FLinearColor& InColor)
+	: Position(InPosition)
+	, Color(InColor)
+{
+}
+
+void FDrawPointCommand::Execute()
+{
+	CCore::Get().GetRenderSystem()->DrawPoint(Position, Color);
+}
---
+++ Engine/Command/DrawPointCommand.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+#include "DrawCommand.h"
+
+class ENGINE_API FDrawPointCommand : public FDrawCommand
+{
+public:
+	FDrawPointCommand(const FVector2 InPosition, const FLinearColor& InColor);
+	virtual ~FDrawPointCommand() = default;
+
+	virtual void Execute() override;
+
+private:
+	FVector2 Position;
+	FLinearColor Color;
+};
