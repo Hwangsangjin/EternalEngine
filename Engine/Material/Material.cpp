8c8d593b034068c77027a938f852fd0084ccd203
Hwangsangjin <hwangsangjin88@gmail.com>
2025-02-15 22:29:31 +0900

    머티리얼 클래스 추가

---
+++ Engine/Material/Material.cpp
@@ -0,0 +1,22 @@
+﻿#include "Precompiled.h"
+#include "Material.h"
+
+FString FMaterial::GetShaderPath(const FString& FileName)
+{
+	// 현재 실행 파일 경로
+	std::filesystem::path CurrentPath = std::filesystem::current_path();
+
+	// 상위 경로로 올라가면서 리소스 폴더 찾기
+	while (CurrentPath.has_parent_path())
+	{
+		// 현재 경로를 상위 경로로 변경
+		CurrentPath = CurrentPath.parent_path();
+
+		// 리소스 폴더를 찾았다면 경로 결합 후 반환
+		if (std::filesystem::exists(CurrentPath / "Resource"))
+			return FString(CurrentPath.wstring().c_str()) + TEXT("/Resource/Shader/") + FileName;
+	}
+
+	// 리소스 폴더를 찾지 못한 경우 빈 문자열 반환
+	return FString();
+}
---
+++ Engine/Material/Material.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+
+class ENGINE_API FMaterial
+{
+public:
+    virtual ~FMaterial() = default;
+
+    virtual void Bind() = 0;
+
+	FString GetShaderPath(const FString& FileName);
+};
