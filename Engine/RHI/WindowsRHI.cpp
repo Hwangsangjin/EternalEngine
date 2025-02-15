616ee1a6e4e0614890ef2635af16db10f570354a
Hwangsangjin <hwangsangjin88@gmail.com>
2025-02-15 22:21:09 +0900

    윈도우즈 RHI 추가

---
+++ Engine/RHI/WindowsRHI.cpp
@@ -0,0 +1,99 @@
+﻿#include "Precompiled.h"
+#include "WindowsRHI.h"
+#include "Window/Window.h"
+
+FWindowsRHI::FWindowsRHI()
+	: Window(CWindow::Get())
+	, WindowHandle(Window.GetWindowHandle())
+	, ScreenSize(Window.GetScreenSize())
+{
+	// 윈도우의 클라이언트 영역 크기 얻기
+	GetClientRect(WindowHandle, &Rect);
+
+	ScreenDC = GetDC(WindowHandle);
+	MemoryDC = CreateCompatibleDC(ScreenDC);
+
+	// 색상 & 비트맵 설정
+	BITMAPINFO BitmapInfo;
+	memset(&BitmapInfo, 0, sizeof(BITMAPINFO));
+	BitmapInfo.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+	BitmapInfo.bmiHeader.biWidth = ScreenSize.X;
+	BitmapInfo.bmiHeader.biHeight = -ScreenSize.Y;
+	BitmapInfo.bmiHeader.biPlanes = 1;
+	BitmapInfo.bmiHeader.biBitCount = 32;
+	BitmapInfo.bmiHeader.biCompression = BI_RGB;
+
+	DIBitmap = CreateDIBSection(MemoryDC, &BitmapInfo, DIB_RGB_COLORS, (void**)&ScreenBuffer, nullptr, 0);
+	DefaultBitmap = reinterpret_cast<HBITMAP>(SelectObject(MemoryDC, DIBitmap));
+
+	// 깊이 버퍼 생성
+	CreateDepthBuffer();
+}
+
+FWindowsRHI::~FWindowsRHI()
+{
+	// 깊이 버퍼 해제
+	if (DepthBuffer)
+	{
+		delete[] DepthBuffer;
+		DepthBuffer = nullptr;
+	}
+
+	// 비트맵 해제
+	SelectObject(MemoryDC, DIBitmap);
+	DeleteObject(DefaultBitmap);
+
+	// 디바이스 컨텍스트 해제
+	DeleteDC(MemoryDC);
+	ReleaseDC(WindowHandle, ScreenDC);
+}
+
+void FWindowsRHI::Clear()
+{
+	FillBuffer();
+	HBRUSH BrushHandle = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	FillRect(MemoryDC, &Rect, BrushHandle);
+	ClearDepthBuffer();
+}
+
+void FWindowsRHI::Present()
+{
+	BitBlt(ScreenDC, 0, 0, ScreenSize.X, ScreenSize.Y, MemoryDC, 0, 0, SRCCOPY);
+}
+
+void FWindowsRHI::FillBuffer()
+{
+	if (!ScreenBuffer)
+		return;
+
+	FColor32* Dest = ScreenBuffer;
+	UINT TotalCount = ScreenSize.X * ScreenSize.Y;
+	FColor32 Color = FLinearColor::White.ToColor32();
+	CopyBuffer<FColor32>(ScreenBuffer, &Color, TotalCount);
+}
+
+void FWindowsRHI::CreateDepthBuffer()
+{
+	DepthBuffer = new float[ScreenSize.X * ScreenSize.Y];
+}
+
+void FWindowsRHI::ClearDepthBuffer()
+{
+	if (DepthBuffer)
+	{
+		float* Dest = DepthBuffer;
+		static float DefValue = INFINITY;
+		UINT TotalCount = ScreenSize.X * ScreenSize.Y;
+		CopyBuffer<float>(DepthBuffer, &DefValue, TotalCount);
+	}
+}
+
+void FWindowsRHI::DrawPoint(const FScreenPoint& InPoint, const FLinearColor& InColor)
+{
+	SetPixel(InPoint, InColor);
+}
+
+void FWindowsRHI::DrawPoint(const FVector2& InPosition, const FLinearColor& InColor)
+{
+	SetPixel(FScreenPoint::ToScreenCoordinate(ScreenSize, InPosition), InColor);
+}
---
+++ Engine/RHI/WindowsRHI.h
@@ -0,0 +1,108 @@
+﻿#pragma once
+
+#include "RHI.h"
+
+class CWindow;
+
+class ENGINE_API FWindowsRHI : public FRHI
+{
+    RTTI_DECL(FWindowsRHI, FRHI)
+
+public:
+    FWindowsRHI();
+    virtual ~FWindowsRHI();
+
+    virtual void Clear() override;
+    virtual void Present() override;
+
+    void FillBuffer();
+
+    FORCEINLINE bool IsInScreen(const FScreenPoint& InPosition) const
+    {
+        if ((InPosition.X < 0 || InPosition.X >= ScreenSize.X) || (InPosition.Y < 0 || InPosition.Y >= ScreenSize.Y))
+            return false;
+
+        return true;
+    }
+
+    FORCEINLINE int GetScreenBufferIndex(const FScreenPoint& InPosition) const
+    {
+        return InPosition.Y * ScreenSize.X + InPosition.X;
+    }
+
+    FORCEINLINE void SetPixel(const FScreenPoint& InPosition, const FLinearColor& InColor)
+    {
+        if (!IsInScreen(InPosition))
+            return;
+
+        FColor32* Dest = ScreenBuffer;
+        *(Dest + GetScreenBufferIndex(InPosition)) = InColor.ToColor32();
+    }
+
+    virtual void DrawPoint(const FVector2& InPosition, const FLinearColor& InColor) override;
+    virtual void DrawPoint(const FScreenPoint& InPoint, const FLinearColor& InColor) override;
+    virtual void DrawLine(const FVector2& InStartPosition, const FVector2& InEndPosition, const FLinearColor& InColor) override {}
+    virtual void DrawText(const FVector2& InPosition, const FString& InText, const FLinearColor& InColor) override {}
+    virtual void DrawStaticMesh(FStaticMesh* InStaticMesh, const std::vector<FMaterial*>& InMaterials) override {}
+
+protected:
+    virtual FVertexBuffer* CreateVertexBuffer(UINT Size, const void* Data) override { return nullptr; }
+    virtual FIndexBuffer* CreateIndexBuffer(UINT Size, const void* Data) override { return nullptr; }
+    virtual void SetVertexBuffer(FVertexBuffer* VertexBuffer, UINT Stride, UINT Offset) override {}
+    virtual void SetIndexBuffer(FIndexBuffer* IndexBuffer) override {}
+
+    FORCEINLINE virtual void* GetRawDevice() const override { return nullptr; }
+    FORCEINLINE virtual void* GetRawDeviceContext() const override { return nullptr; }
+
+private:
+    template <typename T>
+    T* CopyBuffer(T* InDest, T* InSrc, int InCount)
+    {
+        if (InCount == 0)
+            return nullptr;
+
+        if (InCount == 1)
+        {
+            memcpy(InDest, InSrc, sizeof(T));
+        }
+        else
+        {
+            int Half = FMath::FloorToInt(InCount * 0.5f);
+            CopyBuffer<T>(InDest, InSrc, Half);
+            memcpy(InDest + Half, InDest, Half * sizeof(T));
+
+            if (InCount % 2)
+                memcpy(InDest + (InCount - 1), InSrc, sizeof(T));
+        }
+
+        return InDest;
+    }
+
+    void CreateDepthBuffer();
+    void ClearDepthBuffer();
+
+private:
+    // 윈도우
+    CWindow& Window;
+
+    // 윈도우 핸들
+    HWND WindowHandle;
+
+    // 클라이언트 영역
+    RECT Rect;
+
+    // 화면 크기
+    FScreenPoint ScreenSize;
+
+    // 디바이스 컨텍스트
+    HDC ScreenDC;
+    HDC MemoryDC;
+
+    // 버퍼
+    FColor32* ScreenBuffer;
+    float* DepthBuffer;
+
+    // 비트맵
+    HBITMAP DefaultBitmap;
+    HBITMAP DIBitmap;
+};
