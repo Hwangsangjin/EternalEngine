d5d17e9fdb477f12f6dda72ab32d3070b854e2ae
Hwangsangjin <hwangsangjin88@gmail.com>
2025-02-15 22:40:52 +0900

    정적 메시 커맨드 추가

---
+++ Engine/Command/DrawStaticMeshCommand.cpp
@@ -0,0 +1,17 @@
+﻿#include "Precompiled.h"
+#include "DrawStaticMeshCommand.h"
+#include "Mesh/StaticMesh.h"
+#include "Material/Material.h"
+#include "Core/Core.h"
+#include "System/RenderSystem.h"
+
+FDrawStaticMeshCommand::FDrawStaticMeshCommand(FStaticMesh* InStaticMesh, const std::vector<FMaterial*>& InMaterials)
+	: StaticMesh(InStaticMesh)
+	, Materials(InMaterials)
+{
+}
+
+void FDrawStaticMeshCommand::Execute()
+{
+	CCore::Get().GetRenderSystem()->DrawStaticMesh(StaticMesh, Materials);
+}
---
+++ Engine/Command/DrawStaticMeshCommand.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+#include "DrawCommand.h"
+
+class FStaticMesh;
+class FMaterial;
+
+class ENGINE_API FDrawStaticMeshCommand : public FDrawCommand
+{
+public:
+    FDrawStaticMeshCommand(FStaticMesh* InStaticMesh, const std::vector<FMaterial*>& InMaterials);
+    virtual ~FDrawStaticMeshCommand() = default;
+
+    virtual void Execute();
+
+private:
+    FStaticMesh* StaticMesh;
+    std::vector<FMaterial*> Materials;
+};
