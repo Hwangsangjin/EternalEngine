8274baea01480f1d7eb25e89dd6aa11d0c171178
Hwangsangjin <hwangsangjin88@gmail.com>
2025-02-15 22:32:48 +0900

    DX11 인덱스 버퍼 추가

---
+++ Engine/Buffer/DirectX11IndexBuffer.cpp
@@ -0,0 +1,16 @@
+﻿#include "Precompiled.h"
+#include "DirectX11IndexBuffer.h"
+
+FDirectX11IndexBuffer::FDirectX11IndexBuffer(ID3D11Buffer* InIndexBuffer)
+	: IndexBuffer(InIndexBuffer)
+{
+}
+
+FDirectX11IndexBuffer::~FDirectX11IndexBuffer()
+{
+    if (IndexBuffer)
+    {
+        IndexBuffer->Release();
+        IndexBuffer = nullptr;
+    }
+}
---
+++ Engine/Buffer/DirectX11IndexBuffer.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+#include "IndexBuffer.h"
+
+class ENGINE_API FDirectX11IndexBuffer : public FIndexBuffer
+{
+public:
+    FDirectX11IndexBuffer(ID3D11Buffer* InIndexBuffer);
+    virtual ~FDirectX11IndexBuffer();
+
+    virtual void* GetBuffer() const override { return static_cast<void*>(IndexBuffer); }
+
+private:
+    ID3D11Buffer* IndexBuffer;
+};
