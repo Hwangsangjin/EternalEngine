a6c66ed7dec5fb97134163c92929dda1ea036ed1
Hwangsangjin <hwangsangjin88@gmail.com>
2025-02-15 22:39:10 +0900

    텍스트 출력 커맨드 추가

---
+++ Engine/Command/DrawTextCommand.cpp
@@ -0,0 +1,16 @@
+﻿#include "Precompiled.h"
+#include "DrawTextCommand.h"
+#include "Core/Core.h"
+#include "System/RenderSystem.h"
+
+FDrawTextCommand::FDrawTextCommand(const FVector2& InPosition, const FString& InText, const FLinearColor& InColor)
+    : Position(InPosition)
+    , Text(InText)
+    , Color(InColor)
+{
+}
+
+void FDrawTextCommand::Execute()
+{
+    CCore::Get().GetRenderSystem()->DrawText(Position, Text, Color);
+}
---
+++ Engine/Command/DrawTextCommand.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#include "DrawCommand.h"
+
+class ENGINE_API FDrawTextCommand : public FDrawCommand
+{
+public:
+    FDrawTextCommand(const FVector2& InPosition, const FString& InText, const FLinearColor& InColor);
+    virtual ~FDrawTextCommand() = default;
+
+    virtual void Execute() override;
+
+private:
+    FVector2 Position;
+    FString Text;
+    FLinearColor Color;
+};
