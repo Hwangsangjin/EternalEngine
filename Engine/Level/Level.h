05256d3cfe0879c59ee4563f6d95ca7de1a0f330
Hwangsangjin <hwangsangjin88@gmail.com>
2025-02-15 19:52:09 +0900

    함수 선언 위치 수정

--- Engine/Level/Level.h
+++ Engine/Level/Level.h
@@ -12,14 +12,17 @@ public:
 	CLevel();
 	virtual ~CLevel();
 
+	// 루프 처리 함수
+	virtual void Update(float DeltaTime);
+	virtual void Render();
+
 	// 액터 추가, 삭제 함수
 	void AddActor(CActor* InActor);
 	void RemoveActor(CActor* InActor);
 	void RemoveActorFromArray(std::vector<CActor*>& OutActors, CActor* InActor);
 
-	// 루프 처리 함수
-	virtual void Update(float DeltaTime);
-	virtual void Render();
+	// 액터 반환 함수
+	const std::vector<CActor*>& GetActors() const { return Actors; }
 
 protected:
 	// 활성화된 액터의 동적 배열
