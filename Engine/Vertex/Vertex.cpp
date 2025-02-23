8e8b5a855dd6410a491526e9f640e1cdd22dfd74
Hwangsangjin <hwangsangjin88@gmail.com>
2025-02-23 23:40:07 +0900

    텍스처 좌표 추가

--- Engine/Vertex/Vertex.cpp
+++ Engine/Vertex/Vertex.cpp
@@ -1,8 +1,9 @@
 ﻿#include "Precompiled.h"
 #include "Vertex.h"
 
-FVertex::FVertex(const FVector3& InPosition, const FVector3& InColor)
+FVertex::FVertex(const FVector3& InPosition, const FVector3& InColor, const FVector2& InTexCoord)
 	: Position(InPosition)
 	, Color(InColor)
+	, TexCoord(InTexCoord)
 {
 }
--- Engine/Vertex/Vertex.h
+++ Engine/Vertex/Vertex.h
@@ -2,8 +2,9 @@
 
 struct ENGINE_API FVertex
 {
-	FVertex(const FVector3& InPosition, const FVector3& InColor);
+	FVertex(const FVector3& InPosition, const FVector3& InColor, const FVector2& InTexCoord);
 
 	FVector3 Position;
 	FVector3 Color;
+	FVector2 TexCoord;
 };
