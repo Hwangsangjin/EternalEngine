c188b9043383bb2a6f0d0b4194acc3ac205f0b9a
Hwangsangjin <hwangsangjin88@gmail.com>
2025-02-23 23:36:43 +0900

    텍스처 클래스 추가

---
+++ Engine/Texture/Texture.cpp
@@ -0,0 +1,47 @@
+﻿#include "Precompiled.h"
+#include "Texture.h"
+#define STB_IMAGE_IMPLEMENTATION
+#include "Stb/stb_image.h"
+#include "Core/Core.h"
+#include "System/RenderSystem.h"
+#include "RHI/RHI.h"
+
+FTexture::~FTexture()
+{
+	free(Data);
+	Data = nullptr;
+}
+
+void FTexture::LoadTexture(const FString& InTextureName)
+{
+	// 파일 로드
+	Data = stbi_load(GetTexturePath(InTextureName).ToString().c_str(), &Width, &Height, &ChannelCount, 0);
+
+	// 예외 처리
+	if (!Data)
+	{
+		// "Failed to load texture file :"
+		MessageBoxA(nullptr, ("Failed to load texture file :" + Name).ToString().c_str(), "Error", MB_OK);
+		__debugbreak();
+	}
+}
+
+FString FTexture::GetTexturePath(const FString& InFileName)
+{
+	// 현재 실행 파일 경로
+	std::filesystem::path CurrentPath = std::filesystem::current_path();
+
+	// 상위 경로로 올라가면서 리소스 폴더 찾기
+	while (CurrentPath.has_parent_path())
+	{
+		// 현재 경로를 상위 경로로 변경
+		CurrentPath = CurrentPath.parent_path();
+
+		// 리소스 폴더를 찾았다면 경로 결합 후 반환
+		if (std::filesystem::exists(CurrentPath / "Resource"))
+			return FString(CurrentPath.wstring().c_str()) + TEXT("/Resource/Texture/") + InFileName;
+	}
+
+	// 리소스 폴더를 찾지 못한 경우 빈 문자열 반환
+	return FString();
+}
---
+++ Engine/Texture/Texture.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+struct ENGINE_API FTexture
+{
+	virtual ~FTexture();
+
+	virtual void Bind() = 0;
+
+	void LoadTexture(const FString& InTextureName);
+
+	FString GetTexturePath(const FString& InFileName);
+
+	int Width = 0;
+	int Height = 0;
+	int ChannelCount = 0;
+	FString Name;
+	UINT Index = 0u;
+	EBindType BindType = EBindType::PS;
+	void* Data = nullptr;
+};
